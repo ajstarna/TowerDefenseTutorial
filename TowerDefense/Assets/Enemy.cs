45c1b244b6792afaa6ab7369990190ddd8fbb3aa
Adam St Arnaud <ajstarna@ualberta.ca>
2019-11-18 20:49:31 -0700

    restructed project to remove redundant directory

---
+++ TowerDefense/Assets/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int waypoint_index = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[waypoint_index];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (direction.magnitude < 0.3f)
+        {
+            GetNextWaypoint();
+        }
+        
+
+    }
+
+    void GetNextWaypoint()
+    {
+        waypoint_index += 1;
+        if (waypoint_index >= Waypoints.points.Length)
+        {
+            Destroy(gameObject);
+            return; // return so that we do not attempt to find the next target out of range
+        }
+
+        target = Waypoints.points[waypoint_index];
+    }
+
+}
