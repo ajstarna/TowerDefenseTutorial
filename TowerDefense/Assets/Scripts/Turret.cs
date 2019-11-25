a1fe630b85144617f660f5206a1e12f4cfcc3ccc
Adam St Arnaud <ajstarna@ualberta.ca>
2019-11-24 21:44:24 -0700

    done video 4. turrets find nearest target and rotate to face it

---
+++ TowerDefense/Assets/Scripts/Turret.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform target;
+    public float range = 15f;
+    public string enemy_tag = "Enemy";
+    public Transform part_to_rotate;
+    public float turn_speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.33f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemy_tag);
+
+        float closest_distance = Mathf.Infinity;
+        GameObject nearest_enemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distance_to_enemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance_to_enemy < closest_distance)
+            {
+                closest_distance = distance_to_enemy;
+                nearest_enemy = enemy;
+            }
+        }
+
+        if (nearest_enemy != null && closest_distance <= range)
+        {
+            target = nearest_enemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        Quaternion look_rotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(part_to_rotate.rotation, look_rotation, Time.deltaTime * turn_speed).eulerAngles;
+        part_to_rotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
