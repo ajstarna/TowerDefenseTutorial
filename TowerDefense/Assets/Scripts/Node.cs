fcf89665f5206a90ac39024726d7529e7eefceb2
Adam St Arnaud <ajstarna@ualberta.ca>
2019-11-25 21:42:57 -0700

    nodes get darker when mouse over. bout half way through video 6

---
+++ TowerDefense/Assets/Scripts/Node.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hover_color;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color start_color;
+
+    void Start()
+    {
+        start_color = GetComponent<Renderer>().material.color;
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Cannot build on top of existing turret");
+            return;
+        }
+
+        // Build a turret
+        
+    }
+    void OnMouseEnter()
+    {
+        rend.material.color = hover_color;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = start_color;
+    }
+}
